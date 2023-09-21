44304726db2553cc18826bd48875c9574e3f9122
Emanuele Maria <emanuuuelem@gmail.com>
2023-09-20 21:24:04 -0300

    feat: list devedores

---
+++ FRONT-END/devedores/index.php
@@ -0,0 +1,28 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pague os 50 centavos</title>
+    <link rel="stylesheet" href="../globalStyle.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="../img/cash.svg" type="image/svg">
+</head>
+<body>
+    <div class="header">
+        <h1>Devedores</h1>
+        <img src="../img/plus.svg" alt="add payament" width="50">
+    </div>
+    <div class="container">
+
+        <div class="card">
+            <p>João</p>
+            <img src="../img/eye.svg" alt="more details">
+        </div>
+    </div>
+    <footer class="menu">
+        <img src="../img/cash.svg" alt="pagamentos" width="50" height="50">
+        <img src="../img/people.svg" alt="devedores" width="50" height="50">
+        <img src="../img/person.svg" alt="perfil" width="50" height="50">
+    </footer>
+</body>
+</html>
