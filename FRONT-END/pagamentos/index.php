4511153ea091af72399157dac70079238d748c33
Emanuele Maria <emanuuuelem@gmail.com>
2023-09-20 21:03:41 -0300

    feat: page payaments and menu

---
+++ FRONT-END/pagamentos/index.php
@@ -0,0 +1,28 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pague os 50 centavos</title>
+    <link rel="stylesheet" href="../globalStyle.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="../img/cash.svg" type="image/svg">
+</head>
+<body>
+    <div class="header">
+        <h1>Próximos pagamentos</h1>
+        <img src="../img/plus.svg" alt="add payament" width="50">
+    </div>
+    <div class="container">
+
+        <div class="card">
+            <p>João</p>
+            <p>R$500</p>
+        </div>
+    </div>
+    <footer class="menu">
+        <img src="../img/cash.svg" alt="pagamentos" width="50" height="50">
+        <img src="../img/people.svg" alt="devedores" width="50" height="50">
+        <img src="../img/person.svg" alt="perfil" width="50" height="50">
+    </footer>
+</body>
+</html>
